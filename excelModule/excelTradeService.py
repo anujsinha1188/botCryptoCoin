7ba9fa48442255dd114e611d7796d4d5448e896c
artbasket <artbasketrajasthan@gmail.com>
2022-12-04 23:30:56 +0530

    Improvements on automate. Reduce dependency on force check

--- excelModule/excelTradeService.py
+++ excelModule/excelTradeService.py
@@ -5,7 +5,8 @@ import openpyxl
 from excelModule.excelConst import ORDER_ID_STRING, OPEN_TRADE_FILE, EXECUTED_TRADE_FILE, OPEN_TRADE_RULE_SHEET_NAME, \
     LAST_FILLED_ORDER_ADDRESS, REPEAT_RUN_SLEEP_TIME, COIN_LIST, MARKET_STRING, COIN_NAME_ADDRESS, \
     COIN_STATUS_ADDRESS, READY_STRING, OPEN_TRADE_COUNT_ADDRESS, COUNT_STRING, STATUS_COLUMN, TO_BUY_COLUMN, \
-    TO_SELL_COLUMN, QUANTITY_COLUMN, ID_BUY_COLUMN, ID_SELL_COLUMN, STATUS_STRING, FORCE_CHECK_AFTER_LOOP
+    TO_SELL_COLUMN, QUANTITY_COLUMN, ID_BUY_COLUMN, ID_SELL_COLUMN, STATUS_STRING, FORCE_CHECK_AFTER_LOOP, \
+    LAST_ORDER_STATUS_ADDRESS
 from excelModule.excelErrorService import ExcelErrorService
 from excelModule.updateExcelService import UpdateExcelService
 from excelModule.excelBackupService import ExcelBackupService
@@ -21,10 +22,9 @@ class ExcelTradeService:
 
     coinService = CoinService()
 
-    forceRun = True
+    forceRun = False
     repeatRunSleepTime = REPEAT_RUN_SLEEP_TIME
-    orderExecuted = False
-    checkOrderCount = 0
+    checkOrderCount = FORCE_CHECK_AFTER_LOOP * 0
 
     openTradeWorkbook = None
     executedTradeWorkbook = None
@@ -51,18 +51,24 @@ class ExcelTradeService:
 
     def is_open_order_executed(self):
         try:
+            self.get_rule_sheet()
+            self.repeatRunSleepTime = REPEAT_RUN_SLEEP_TIME
             order_executed = False
             response_trade_history = self.coinService.get_account_trade_history()
             trade_id = response_trade_history[0].get(ORDER_ID_STRING)
-            response_order_status = self.coinService.check_order_status(trade_id)
-            if response_order_status.get("status") == "filled":
-                if trade_id != self.openTradeSheet[LAST_FILLED_ORDER_ADDRESS].value:
+
+            if trade_id != self.openTradeSheet[LAST_FILLED_ORDER_ADDRESS].value:
+                order_executed = True
+                self.openTradeSheet[LAST_FILLED_ORDER_ADDRESS].value = trade_id
+
+            elif self.openTradeSheet[LAST_ORDER_STATUS_ADDRESS].value != "filled":
+
+                response_order_status = self.coinService.check_order_status(trade_id)
+                trade_status = response_order_status.get("status")
+
+                if trade_status != self.openTradeSheet[LAST_ORDER_STATUS_ADDRESS].value:
                     order_executed = True
-                    self.openTradeSheet[LAST_FILLED_ORDER_ADDRESS].value = trade_id
-                self.repeatRunSleepTime = REPEAT_RUN_SLEEP_TIME
-            else:
-                self.repeatRunSleepTime = REPEAT_RUN_SLEEP_TIME * 4
-                self.forceRun = True
+                    self.openTradeSheet[LAST_ORDER_STATUS_ADDRESS].value = trade_status
 
             return order_executed
         except Exception as error:
@@ -71,17 +77,20 @@ class ExcelTradeService:
     def start_job(self):
         resume_check = True
         self.get_workbook()
-        self.get_rule_sheet()
 
         while resume_check:
-            self.checkOrderCount += 1
+            # self.checkOrderCount += 1
+
             if self.checkOrderCount > FORCE_CHECK_AFTER_LOOP:
                 self.forceRun = True
                 self.checkOrderCount = 0
+            else:
+                self.forceRun = False
+
             if self.forceRun or self.is_open_order_executed():
                 self.run_job()
             else:
-                print("No open trade executed.")
+                print("No open trade filled.")
 
             print("Info: Sleep for " + str(self.repeatRunSleepTime) + " seconds.")
             time.sleep(self.repeatRunSleepTime)
@@ -89,40 +98,34 @@ class ExcelTradeService:
             time.sleep(1)
 
     def run_job(self):
-        self.orderExecuted = False
+
         for coin in COIN_LIST:
             self.coin_tradeCode = coin.get(MARKET_STRING)
 
             try:
                 self.openTradeSheet = self.openTradeWorkbook[self.coin_tradeCode]
 
-                response_order_count = self.coinService.get_active_order_count(self.coin_tradeCode)
-                count = response_order_count.get(COUNT_STRING)
-                if self.forceRun:
-                    print("Force running for: " + self.coin_tradeCode)
-                    self.openTradeSheet[OPEN_TRADE_COUNT_ADDRESS].value = count
-                    self.process_coin()
-                else:
-                    if self.openTradeSheet[COIN_NAME_ADDRESS].value == self.coin_tradeCode \
-                            and self.openTradeSheet[COIN_STATUS_ADDRESS].value == READY_STRING:
-                        if count != self.openTradeSheet[OPEN_TRADE_COUNT_ADDRESS].value:
-                            print("Running: " + self.coin_tradeCode + ". Trade count: " + str(count))
-                            self.openTradeSheet[OPEN_TRADE_COUNT_ADDRESS].value = count
-                            self.openTradeWorkbook.save(OPEN_TRADE_FILE)
-                            self.process_coin()
-                        else:
-                            print("No change in trade count: " + self.coin_tradeCode +
-                                  ". Trade count: " + str(count) + ".")
+                if self.openTradeSheet[COIN_NAME_ADDRESS].value == self.coin_tradeCode \
+                        and self.openTradeSheet[COIN_STATUS_ADDRESS].value == READY_STRING:
 
+                    response_order_count = self.coinService.get_active_order_count(self.coin_tradeCode)
+                    count = response_order_count.get(COUNT_STRING)
+
+                    if self.forceRun or count != self.openTradeSheet[OPEN_TRADE_COUNT_ADDRESS].value:
+                        print("Running: " + self.coin_tradeCode + ". Trade count: " + str(count))
+                        self.openTradeSheet[OPEN_TRADE_COUNT_ADDRESS].value = count
+                        self.openTradeWorkbook.save(OPEN_TRADE_FILE)
+                        self.process_coin()
+                        if self.forceRun:
+                            print("Sleep for 1 seconds in Force run.")
+                            time.sleep(1)
+                    else:
+                        print("No force run or change in trade count: " + self.coin_tradeCode +
+                              ". Trade count: " + str(count) + ".")
+                else:
+                    print("Sheet " + self.coin_tradeCode + " is not ready.")
             except Exception as error:
                 self.excelErrorService.handel_error(error)
-
-        if self.orderExecuted:
-            self.repeatRunSleepTime = 1
-            self.checkOrderCount = 0
-            self.forceRun = True
-        else:
-            self.forceRun = False
 
     def process_coin(self):
         self.openTradeRow = 4
@@ -147,7 +150,7 @@ class ExcelTradeService:
                 end_of_file = True
 
     def order_coin(self, side):
-        self.orderExecuted = True
+        self.repeatRunSleepTime = 1
         market = self.coin_tradeCode
         price_per_unit = None
         response = None
@@ -158,15 +161,21 @@ class ExcelTradeService:
             price_per_unit = self.openTradeSheet[TO_SELL_COLUMN + str(self.openTradeRow)].value
 
         total_quantity = self.openTradeSheet[QUANTITY_COLUMN + str(self.openTradeRow)].value
+
         try:
+
             response = self.coinService.order_coin(side, market, price_per_unit, total_quantity)
+
             if side == "buy":
                 self.updateExcelService. \
                     update_buy_coin(response, self.openTradeSheet, self.openTradeRow)
             elif side == "sell":
                 self.updateExcelService. \
                     update_sell_coin(response, self.openTradeSheet, self.openTradeRow)
+
+            self.openTradeSheet[OPEN_TRADE_COUNT_ADDRESS].value += 1
             self.openTradeWorkbook.save(OPEN_TRADE_FILE)
+
         except Exception as error:
             if response is not None and response.get(STATUS_STRING) == "error":
                 print(str(error))
@@ -185,16 +194,23 @@ class ExcelTradeService:
             order_id = self.openTradeSheet[ID_SELL_COLUMN + str(self.openTradeRow)].value
 
         try:
+
             response = self.coinService.check_order_status(order_id)
+
             if response.get('status') == "error":
-                self.updateExcelService. \
-                    update_error_status(response, self.openTradeSheet, self.openTradeRow)
-                self.openTradeWorkbook.save(OPEN_TRADE_FILE)
+
+                time.sleep(30)
+                if response.get('code') == 400:
+                    self.updateExcelService.update_error_status(response, self.openTradeSheet, self.openTradeRow)
+                    self.openTradeWorkbook.save(OPEN_TRADE_FILE)
+
             elif response.get("status") == "filled":
+
                 if side == "buy":
                     self.updateExcelService.update_buy_order(response, self.openTradeSheet, self.openTradeRow)
                     self.openTradeWorkbook.save(OPEN_TRADE_FILE)
                     self.order_coin("sell")
+
                 elif side == "sell":
                     self.assign_executed_trade_sheet(response)
                     self.executedTradeSheet.insert_rows(6)
@@ -204,7 +220,9 @@ class ExcelTradeService:
                     self.openTradeWorkbook.save(OPEN_TRADE_FILE)
                     self.executedTradeWorkbook.save(EXECUTED_TRADE_FILE)
                     self.order_coin("buy")
+
                 self.excelBackupService.create_backup()
+
         except Exception as error:
             self.excelErrorService.handel_error(error)
 
